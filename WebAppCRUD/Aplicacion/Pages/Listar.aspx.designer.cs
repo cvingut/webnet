46aff45d3f3d3f7880c61d002136abd04d5e5828
cvingut <cvingut.es@gmail.com>
2016-01-09 16:52:10 +0100

    v stable css 2016.01.09.16.52

--- WebAppCRUD/Aplicacion/Pages/Listar.aspx.designer.cs
+++ WebAppCRUD/Aplicacion/Pages/Listar.aspx.designer.cs
@@ -11,5 +11,32 @@ namespace WebApp.Aplication.Account {
     
     
     public partial class Listar {
+        
+        /// <summary>
+        /// Control fpUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm fpUsuarios;
+        
+        /// <summary>
+        /// Control DSUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource DSUsuarios;
+        
+        /// <summary>
+        /// Control gvUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvUsuarios;
     }
 }
