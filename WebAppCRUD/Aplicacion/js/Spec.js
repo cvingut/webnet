cac02aaa8bda0ea104c8e0f5a95078b4a7480c5e
vingut <vingut@VINGUT-PC>
2016-09-21 16:24:31 +0200

    CHJS

--- WebAppCRUD/Aplicacion/js/Spec.js
+++ WebAppCRUD/Aplicacion/js/Spec.js
@@ -4,3 +4,26 @@
     $(".Spec_Selected").removeClass("Spec_Selected");
     $(classElement).addClass("Spec_Selected");
 }
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("id", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("id");
+    if (data != null) {
+        var element = document.getElementById(data);
+        var relement = element.cloneNode(true);
+        relement.draggable = false;
+        ev.target.innerHTML = "";
+        ev.target.appendChild(relement);
+
+    }
+
+}
